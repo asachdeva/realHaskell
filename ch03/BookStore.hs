0d46699d5f0cce8836063abaadb5884e6e5358ee
Akshay Sachdeva <asachdeva@alumni.utexas.net>
2024-11-13 12:20:59 -0600

    vim-hindent

--- ch03/BookStore.hs
+++ ch03/BookStore.hs
@@ -1,22 +1,38 @@
 -- file: ch03/BookStore.hs
-
 -- type synonyms
 type CustomerID = Int
+
 type ReviewBody = String
+
 type CardHolder = String
+
 type CardNumber = String
+
 type Address = [String]
 
-data BookInfo = Book Int String [String]
-                 deriving (Show)
+data BookInfo =
+  Book Int String [String]
+  deriving (Show)
 
-data BookReview = BookReview BookInfo CustomerID String
-data BetterReview = BetterReview BookInfo CustomerID ReviewBody
+data BookReview =
+  BookReview BookInfo CustomerID String
 
-data BillingInfo = CreditCard CardNumber CardHolder Address
-                 | CashOnDelivery
-                 | Invoice CustomerID
-                   deriving (Show)
+data BetterReview =
+  BetterReview BookInfo CustomerID ReviewBody
+
+data BillingInfo
+  = CreditCard CardNumber CardHolder Address
+  | CashOnDelivery
+  | Invoice CustomerID
+  deriving (Show)
 
 type BookRecord = (BookInfo, BetterReview)
 
+bookID (Book id title authors) = id
+
+bookTitle (Book id title authors) = title
+
+bookAuthors (Book id title authors) = authors
+
+data BookRecord2 =
+  (,) BookInfo BetterReview
--- ch03/add.hs
+++ ch03/add.hs
@@ -1,3 +1,10 @@
 -- file: ch03/ad.hs
 add a b = a + b
 
+myNot True = False
+myNot False = True
+
+sumList (x:xs) = x + sumList xs
+sumList [] = 0
+
+
